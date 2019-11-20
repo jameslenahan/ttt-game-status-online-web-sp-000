1f7fa8321fa7b4d2afb988b9bc1295aaa9a01faa
James Lenahan <james.k.lenahan@gmail.com>
2019-11-20 19:05:15 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,12 +17,3 @@ WIN_COMBINATIONS = [
 ]
 
 
-def WIN_COMINATIONS(board)
-  if board[top_row_win[0]] == "X" &&
-  board[top_row_win[1]] == "X" &&
-  board[top_row_win[2]] == "X"
-    "X won in the top row"
-    
-  
-  end
-end
