6c2a837dbf6ca649b32331b59450c90036125bad
James Lenahan <james.k.lenahan@gmail.com>
2019-11-20 19:52:59 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,7 +17,7 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  for each win_combination in {|WIN_COMBINATIONS|
+  for each win_combination in {|WIN_COMBINATIONS|}
     win_index_0 = win_combination[1]
     win_index_1 = win_combination[2]
     win_index_2 = win_combination[3]
@@ -31,6 +31,5 @@ def won?(board)
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
       return win_combination
     end
-  }
   return false
 end
