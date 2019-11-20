d89d1e33871f5b044d22cf08e68e860b1a0589b0
James Lenahan <james.k.lenahan@gmail.com>
2019-11-20 19:47:32 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -24,7 +24,7 @@ def won?(board)
     
     position_1 = board[win_index_0]
     position_2 = board[win_index_1]
-    position_3 = board[win_index_2]
+    to_i.position_3 = board[win_index_2]
     
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
       return win_combination
