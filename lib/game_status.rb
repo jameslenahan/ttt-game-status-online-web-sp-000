19caeac4ada168efaab88bf1dcb0495dcfa7ec2c
James Lenahan <james.k.lenahan@gmail.com>
2019-11-20 19:00:26 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,6 +8,9 @@ WIN_COMBINATIONS = [
   [0,1,2] #top row
   [3,4,5] #middle row
   [6,7,9] #bottom row
+  [0,4,9] #left>right diagonal
+  [2,4,6] #right>left diagonal
+  [0,3,6] #left row 
   
   
   
