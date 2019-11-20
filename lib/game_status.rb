336cf67457578b5095540ef75e7619a495e8b598
James Lenahan <james.k.lenahan@gmail.com>
2019-11-20 19:04:33 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,6 +5,7 @@ end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
+  
   [0,1,2] #top row
   [3,4,5] #middle row
   [6,7,9] #bottom row
@@ -13,12 +14,7 @@ WIN_COMBINATIONS = [
   [0,3,6] #left column
   [1,4,7] #middle column
   [3,5,9] #right column
-  
-  
-  
-  
-  
-  ]
+]
 
 
 def WIN_COMINATIONS(board)
