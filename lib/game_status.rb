1b5ffd596ddb3c3bad523c3c9235ffea15f9d6a5
James Lenahan <james.k.lenahan@gmail.com>
2019-11-20 19:08:09 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,12 +8,12 @@ WIN_COMBINATIONS = [
   
   [0, 1, 2], #top row
   [3, 4, 5], #middle row
-  [6, 7, 9], #bottom row
-  [9, 4, 0], #left>right diagonal
+  [6, 7, 8], #bottom row
+  [8, 4, 0], #left>right diagonal
   [6, 4, 2], #right>left diagonal
   [0, 3, 6], #left column
   [1, 4, 7], #middle column
-  [2, 5, 9]  #right column
+  [2, 5, 8]  #right column
 ]
 
 
