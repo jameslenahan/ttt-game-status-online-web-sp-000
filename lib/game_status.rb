74ce647e4375a585885f7e1d00318a39844d085b
James Lenahan <james.k.lenahan@gmail.com>
2019-11-20 18:54:46 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,5 +4,11 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+
 def WIN_COMINATIONS(board)
-  if top_row_win
+  if board[top_row_win[0]] == "X" &&
+  board[top_row_win[1]] == "X" &&
+  board[top_row_win[2]] == "X"
+    "X won in the top row"
+  end
+end
