05fa4a6b259d7850fdf747c743f4f7af32ad0110
James Lenahan <james.k.lenahan@gmail.com>
2019-11-20 20:20:35 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -39,6 +39,13 @@ def full?(board)
  board.all? {|index| index == "X" || index == "O"}
 end
 
+def draw?(board)
+  if !won?(board) && full?(board)
+    return true
+  else
+    return false
+  end
+end
 
 def over?(board)
   if !won?(board) && full?(board)
